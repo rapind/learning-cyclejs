eecd6422da4ddab33dca727410eb1bf77c7c5a8e
Dave Rapin <dave@rapin.com>
2016-03-16 09:00:36 -0400

    Isolated components

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import Rx from 'rx'
 import Cycle from '@cycle/core'
 import CycleDOM from '@cycle/dom'
+import CycleIsolate from '@cycle/isolate'
+const isolate = CycleIsolate
 
 const { makeDOMDriver } = CycleDOM
 const { div, input, label } = CycleDOM
@@ -13,8 +15,7 @@ function intent (DOMSource) {
 }
 
 function model (newValue$, props$) {
-  const initialValue$ = props$
-    .map(props => props.init).first()
+  const initialValue$ = props$.map(props => props.init).first()
   const value$ = initialValue$.concat(newValue$)
 
   return Rx.Observable.combineLatest(
@@ -56,16 +57,48 @@ function LabeledSlider (sources) {
   }
 }
 
-function main (sources) {
-  const props$ = Rx.Observable.of({
-    label: 'Height',
-    unit: 'cm',
-    min: 140,
-    max: 220,
-    init: 170
-  })
+function IsolatedLabeledSlider (sources) {
+  return isolate(LabeledSlider)(sources)
+}
 
-  return LabeledSlider({ DOM: sources.DOM, props: props$ })
+function main (sources) {
+  const weightSinks = IsolatedLabeledSlider({
+    DOM: sources.DOM,
+    props: Rx.Observable.of({
+      label: 'Weight',
+      unit: 'kg',
+      min: 40,
+      max: 150,
+      init: 70
+    })
+  })
+  const weightVTree$ = weightSinks.DOM
+
+  const heightSinks = IsolatedLabeledSlider({
+    DOM: sources.DOM,
+    props: Rx.Observable.of({
+      label: 'Height',
+      unit: 'cm',
+      min: 140,
+      max: 220,
+      init: 170
+    })
+  })
+  const heightVTree$ = heightSinks.DOM
+
+  const vtree$ = Rx.Observable.combineLatest(
+    weightVTree$,
+    heightVTree$,
+    (weightVTree, heightVTree) =>
+      div([
+        weightVTree,
+        heightVTree
+      ])
+  )
+
+  return {
+    DOM: vtree$
+  }
 }
 
 // Effects (imperative)
